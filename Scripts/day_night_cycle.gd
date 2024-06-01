1c5f4c1de518539df5e2cbb0aa0acce60b07e746
Aarogya Yadav <aky104@student.bham.ac.uk>
2024-06-01 19:27:11 +0100

    Random fish generation and fish movement implemented

--- Scripts/day_night_cycle.gd
+++ Scripts/day_night_cycle.gd
@@ -6,5 +6,5 @@ var time = 0.0
 
 func _process(delta):
 	time += delta/20
-	var value = (sin(time - PI / 2) + 1.0) /2.0
+	var value = (sin(time ) + 1.0) /2.0
 	color = gradient.gradient.sample(value)
